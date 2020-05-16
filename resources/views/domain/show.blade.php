86887d0ee9d00bc4949fb4d1e1d0c29ac9e42b98
rinat salimyanov <rinatsin@gmail.com>
2020-05-16 18:21:33 +0500

    Add view

--- resources/views/domain/show.blade.php
+++ resources/views/domain/show.blade.php
@@ -4,16 +4,20 @@
 <h2>Site: {{  $domain[0]->name }}</h2>
 <table>
     <tr>
-        <td>id</td><td>{{  $domain[0]->id }}</td>
+        <td>id</td>
+        <td>{{  $domain[0]->id }}</td>
     </tr>
     <tr>
-        <td>name</td><td>{{ $domain[0]->name }}</td>
+        <td>name</td>
+        <td>{{ $domain[0]->name }}</td>
     </tr>
     <tr>
-        <td>created_at</td><td>{{ $domain[0]->created_at }}</td>
+        <td>created_at</td>
+        <td>{{ $domain[0]->created_at }}</td>
     </tr>
     <tr>
-        <td>updated_at</td><td>{{ $domain[0]->updated_at }}</td>
+        <td>updated_at</td>
+        <td>{{ $domain[0]->updated_at }}</td>
     </tr>
 </table>
 @endsection
